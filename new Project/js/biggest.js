e773a8a51eb0478babd849f63d6fca0668cb544a
musengwat <loganstucki@gmail.com>
2018-04-15 12:58:05 -0500

    add everything ive worked on since the beginning

---
+++ new Project/js/biggest.js
@@ -0,0 +1,41 @@
+
+
+function asdf() {
+    console.log("clicked");
+    alert("you now have the big gay");
+}
+    var open = false;
+
+
+function popDown(){
+    console.log("=====================")
+    console.log("before = " + open);
+    if (open == false){
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("rotate").classList.remove("top-reverse");
+    document.getElementById("scale").classList.remove("scale-down");
+    document.getElementById("move").classList.remove("move-down");
+        
+    document.getElementById("rotate").classList.add("top-active");
+    document.getElementById("scale").classList.add("scale-up");
+    document.getElementById("move").classList.add("move-up");
+
+    open = true;
+    }
+    else{
+    document.getElementById("rotate").classList.remove("top-active");        
+    document.getElementById("scale").classList.remove("scale-up");
+    document.getElementById("move").classList.remove("move-up");
+        
+    document.getElementById("rotate").classList.add("top-reverse");
+    document.getElementById("scale").classList.add("scale-down");
+    document.getElementById("move").classList.add("move-down");
+    open = false;
+//    setTimeout(function(){  document.getElementById("overlay").style.display = "none"; }, 3000);
+
+    }
+    console.log("after = " + open);
+}
+
+
+   
---
+++ new Project/you now have the double gay_files/biggest.js
@@ -0,0 +1,31 @@
+
+function asdf() {
+    console.log("clicked");
+    alert("you now have the big gay");
+}
+
+function pop(){
+    document.getElementById("menu-1").style.width = "33px";
+    document.getElementById("menu-1").style.height = "3px";
+    document.getElementById("menu-2").style.width = "33px";
+    document.getElementById("menu-2").style.height = "3px";
+    document.getElementById("menu-3").style.width = "33px";
+    document.getElementById("menu-3").style.height = "3px";
+    
+    setTimeout(function(){ 
+        document.getElementById("menu-1").style.width = "30px";
+    document.getElementById("menu-1").style.height = "2px";
+    document.getElementById("menu-2").style.width = "30px";
+    document.getElementById("menu-2").style.height = "2px";
+    document.getElementById("menu-3").style.width = "30px";
+    document.getElementById("menu-3").style.height = "2px";
+ }, 70);
+   
+//    alert("pop");
+}
+
+function popDown(){
+    pop();
+    s
+    
+}
